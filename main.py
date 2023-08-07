dddae0b6208376aae04b3798a1ba6a08ce8ce682
JAAR <joe.aaron.acosta@outlook.com>
2023-08-07 14:54:04 -0400

    Version 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 >>> JAAR
 >>> 8/7/2023
 >>> Practicing Fundamentals Program 14
->>> Version 0
+>>> Version 1
 '''
 
 '''
@@ -10,7 +10,13 @@
 '''
 
 def main() :
-    pass
+    with open('the_little_prince.txt', 'r') as file :
+        with open('copy.txt', 'w') as new_file :
+            for line in file.readlines() :
+                new_file.write(line)
+
+    with open('copy.txt', 'r') as copy :
+        print(copy.read())
 
 if __name__ == '__main__' :
     main()
