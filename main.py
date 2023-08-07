0926bf546da06cf94de999625fac414987bc3207
JAAR <joe.aaron.acosta@outlook.com>
2023-08-07 14:47:56 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 8/7/2023
+>>> Practicing Fundamentals Program 14
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that takes a text file and copies the contents of that file to another file.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
